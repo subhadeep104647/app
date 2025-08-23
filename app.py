c7f0b367806de682090ee8da46e0fc752bc43254
SUBHADEEP BISWAS <147643078+subhadeep104647@users.noreply.github.com>
2025-08-24 00:21:10 +0530

    done

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def login():
+    return render_template("login.html")
 
+@app.route("/submit", methods=["POST"])
+def submit():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    if username == "subho" and password == "123":
+        return render_template("home.html", name=username)
+    
+    """else:
+        return "invalid !"""
+    
+
+
+    
 if __name__ == "__main__":
     app.run(debug=True)
