f16c927a50364fe6da79a94d0739b1b3b813f0ef
SUBHADEEP BISWAS <147643078+subhadeep104647@users.noreply.github.com>
2025-08-23 20:48:17 +0530

    html done

--- app.py
+++ app.py
@@ -3,12 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Hello, Flask! 🚀"
-
-@app.route("/about")
-def about():
-    return "This is the About page."
+def login():
 
 if __name__ == "__main__":
     app.run(debug=True)
